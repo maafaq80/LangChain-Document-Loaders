f504caaa93f460f2212efc093f1ffea5b3a71fa8
muhammad aafaq <maafaq.mscs23seecs@seecs.edu.pk>
2025-06-24 16:24:39 +0500

    Directory_loader with Load()

--- directory_loader.py
+++ directory_loader.py
@@ -1,14 +1,19 @@
 # from langchain_openai import ChatOpenAI
 # from langchain_core.prompts import PromptTemplate
 # import os 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 # from langchain_core.output_parsers import StrOutputParser
 # from langchain_core.runnables import RunnableLambda
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import DirectoryLoader
 
-loader=PyPDFLoader('research.pdf')
-result=loader.load()
-print(len(result))
 
-# print(result)
+loader=DirectoryLoader(
+    path='papers',
+    glob='*.pdf',
+    loader_cls=PyPDFLoader
+)
+
+docs=loader.load()
+print(len(docs))
+print(docs)
